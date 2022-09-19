6d87bb65feb1ddbc9be2500050cf5f83faac5940
Sirapakit <sirapakit.lim@student.mahidol.edu>
2022-09-20 05:20:10 +0700

    finish ass-4

---
+++ Assignment-4/thresholding.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+# image is loaded with imread command
+image = cv2.imread('../image1.jpg')
+
+# convert the image in grayscale
+img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# applying different thresholding
+ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 120, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO_INV)
+
+cv2.imshow('Original', image)
+cv2.imshow('Binary Threshold', thresh1)
+cv2.imshow('Binary Threshold Inverted', thresh2)
+cv2.imshow('Truncated Threshold', thresh3)
+cv2.imshow('Set to 0', thresh4)
+cv2.imshow('Set to 0 Inverted', thresh5)
+
+# De-allocate any associated memory usage
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
