c2d06a3b4bb001b77bc1ad813c64c209e42f6870
Sirapakit <sirapakit.lim@student.mahidol.edu>
2022-09-20 22:17:37 +0700

    task-4

--- Assignment-4/histogram-equlization.py
+++ Assignment-4/histogram-equlization.py
@@ -8,22 +8,20 @@ hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 
 cdf = hist.cumsum()
 cdf_normalized = cdf * float(hist.max()) / cdf.max()
-
-plt.plot(cdf_normalized, color='b')
-plt.hist(img.flatten(), 256, [0, 256], color='r')
+plt.figure()
+plt.hist(img.flatten(), 256, [0, 256])
 plt.xlim([0, 256])
-plt.legend(('cdf', 'histogram'), loc='upper left')
 
 cdf_m = np.ma.masked_equal(cdf, 0)
 cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
 cdf = np.ma.filled(cdf_m, 0).astype('uint8')
 img2 = cdf[img]
-
-plt.plot(cdf, color='b')
-plt.hist(img.flatten(), 256, [0, 256], color='r')
+plt.figure()
+plt.hist(img2.flatten(), 256, [0, 256])
 plt.xlim([0, 256])
-plt.legend(('cdf', 'histogram'), loc='upper left')
 
 cv.imshow('img', img)
 cv.imshow('img2', img2)
+cv.imwrite('low-contrast-2.jpg', img2)
 plt.show()
+cv.waitKey(0)
--- Assignment-4/thresholding.py
+++ Assignment-4/thresholding.py
@@ -7,20 +7,28 @@ image = cv2.imread('../image1.jpg')
 # convert the image in grayscale
 img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# applying different thresholding
+#  different thresholding
 ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
 ret, thresh2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY_INV)
 ret, thresh3 = cv2.threshold(img, 120, 255, cv2.THRESH_TRUNC)
 ret, thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
 ret, thresh5 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO_INV)
 
-cv2.imshow('Original', image)
 cv2.imshow('Binary Threshold', thresh1)
 cv2.imshow('Binary Threshold Inverted', thresh2)
 cv2.imshow('Truncated Threshold', thresh3)
 cv2.imshow('Set to 0', thresh4)
 cv2.imshow('Set to 0 Inverted', thresh5)
 
-# De-allocate any associated memory usage
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+cv2.imshow('be4 Thres', img)
+
+# manual thres
+height, width = img.shape
+for i in range(height):
+    for j in range(width):
+        if np.sum(img[i, j]) <= 200:
+            img[i, j] = 0
+cv2.imshow('Thres', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
